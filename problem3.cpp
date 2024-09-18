1e4821f4861d903d23b3540741a73a9735eba029
GianV-s <giancarlo.villachica@ucsp.edu.pe>
2024-09-18 08:09:20 -0500

    Add files via upload

---
+++ problem3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+//1,3,5,7,9
+int main(){
+    int n{0};
+    int i{3};
+    cout << "Ingrese un numero entero: ";
+        cin >> n;
+    if(n % 2 == 0){
+        cout << 1;
+        while (i < n){
+            cout << "," << i;
+            i += 2;
+        }
+    }
+    else{
+        cout << 1;
+        while (i < n){
+            cout << "," << i;
+            i += 2;
+        }
+    }
+}
